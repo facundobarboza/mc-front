e95b84c7f940a4d2f381b8cad0c173605d80a796
facundobarboza <facundo.barboza@outlook.com>
2020-04-30 02:26:34 -0300

    Ruta principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,7 +14,7 @@ import { LoginComponent } from './login/login.component';
 
 const APP_ROUTES: Routes = [
     {
-        path: '',
+        path: 'home',
         component: PagesComponent,
         canActivate: [AuthGuard],
         children: [
