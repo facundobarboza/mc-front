adb2c36264271fc33ebb9326781c540fa399bdf2
facundobarboza <facundo.barboza@outlook.com>
2020-05-03 07:56:26 -0300

    Acualizacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDdi7J1Q8s9sNaI-Owu9Amjnwtdaw3UPgw",
+    authDomain: "mc-front.firebaseapp.com",
+    databaseURL: "https://mc-front.firebaseio.com",
+    projectId: "mc-front",
+    storageBucket: "mc-front.appspot.com",
+    messagingSenderId: "724615159565",
+    appId: "1:724615159565:web:144c1f293527d95b675a44",
+    measurementId: "G-G3BXX8B2YX"
+  }
 };
